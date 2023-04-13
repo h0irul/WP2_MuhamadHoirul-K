6f1b176aa215c70dc92aac19faa211f10c65283d
h0irul <mallbdoh@gmail.com>
2023-04-13 16:11:42 +0700

    First

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Muhamad Hoirul K 
+            Saya tinggal di daerah Cikarang selatan
+            sesuatu hal yang tidak saya sukai adalah 
+            Kemunafikan";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,20 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhamad </li>
+        <li>Nama Belakang : Hoirul </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Cikarang</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Lari</li>
+        <li>Bulutangkis</li>
+        <li>Renang</li>
+    </ul>
+</section>
